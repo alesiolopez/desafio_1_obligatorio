c1c33d0db0b0bcead4dc44d912ad9b335f259808
Alesio López <alesiolopez26@gmail.com>
2022-08-16 14:17:04 -0300

    Corto el código y lo ingreso al condicional

--- js/script.js
+++ js/script.js
@@ -6,22 +6,22 @@ function Solicitar_Prestamo(){
     monto = document.getElementById("monto_input").value;
     meses = document.getElementById("meses_input").value;
 
-    //Cambio el formato de la moneda de la variable.
-    let monto_formato = Intl.NumberFormat('ARS', { style: 'currency', currency: 'ARS' }).format(monto);
-    
-    //Realizo los cálculos de los intereses sobre los meses. Y les doy formato moneda a la variable.
-    let prestamo_mes_interes = parseFloat((monto / meses) * 1.54);
-    let prestamo_mes_interes_formato = Intl.NumberFormat('ARS', { style: 'currency', currency: 'ARS' }).format(prestamo_mes_interes);
-    
-    let total_a_pagar = prestamo_mes_interes * meses;
-    let total_a_pagar_formato = Intl.NumberFormat('ARS', { style: 'currency', currency: 'ARS' }).format(total_a_pagar);
-
     //Igreso un condicional para que le diga al usuario que debe ingresar datos antes de continuar. Luego muestra los cálculos en el html.
     if(monto == "" && meses == "" || monto == "" || meses == ""){
         document.getElementById("mostrar").innerHTML = `
         COMPLETE LOS CAMPOS PARA GENERAR EL CÁLCULO.
         `
     } else{
+        //Cambio el formato de la moneda de la variable.
+        let monto_formato = Intl.NumberFormat('ARS', { style: 'currency', currency: 'ARS' }).format(monto);
+        
+        //Realizo los cálculos de los intereses sobre los meses. Y les doy formato moneda a la variable.
+        let prestamo_mes_interes = parseFloat((monto / meses) * 1.54);
+        let prestamo_mes_interes_formato = Intl.NumberFormat('ARS', { style: 'currency', currency: 'ARS' }).format(prestamo_mes_interes);
+        
+        let total_a_pagar = prestamo_mes_interes * meses;
+        let total_a_pagar_formato = Intl.NumberFormat('ARS', { style: 'currency', currency: 'ARS' }).format(total_a_pagar);
+
         document.getElementById("mostrar").innerHTML = `
         Monto solicitado: <b>${monto_formato}</b>. <br>
         Plazo de pago: <b>${meses}</b> meses. <br>
